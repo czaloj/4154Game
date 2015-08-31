ccc5126b928b6171052f1e0c5a4dcb49d6faa81d
czaloj <czaloj@gmail.com>
2015-08-31 10:02:30 -0400

    Animations can delay certain frame counts

--- Game/src/Animated.hx
+++ Game/src/Animated.hx
@@ -8,19 +8,26 @@ class Animated extends Image {
     private var ss:SpriteSheet;
     private var strip:SpriteStrip;
     private var f:Int = 0;
+    private var fDelay:Int = 1;
+    private var delay:Int = 0;
     
-    public function new(s:SpriteSheet, strip:String) {
+    public function new(s:SpriteSheet, strip:String, delay:Int = 1) {
         super(s.texture);
         ss = s;
         this.strip = ss.getStrip(strip);
         width = this.strip.width;
         height = this.strip.height;
+        this.delay = delay;
         addEventListener(Event.ENTER_FRAME, update);
     }
     
     private function update(e:Event = null):Void {
-        f++;
-        f %= 10;
-        strip.setToFrame(this, f);
+        fDelay--;
+        if (fDelay <= 0) {
+            fDelay = delay;
+            f++;
+            f %= strip.totalFrames;
+            strip.setToFrame(this, f);
+        }
     }
 }
