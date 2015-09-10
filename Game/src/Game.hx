4ef2386336b3398174f978d5ea117a7a597a35ff
czaloj <czaloj@gmail.com>
2015-09-10 08:52:17 -0400

    3 animations

--- Game/src/Game.hx
+++ Game/src/Game.hx
@@ -22,25 +22,28 @@ class Game extends Sprite {
         openfl.Lib.current.stage.addEventListener(MouseEvent.CLICK, add);
     }
     
-    private function randomAnimation():String {
-        switch(Std.int(Math.random() * 2.0)) {
+    private function randomAnimation():Animated {
+        switch(Std.int(Math.random() * 3.0)) {
         case 0:
-            return "Walking";
+            return new Animated(sheet, "Walking", 2);
+        case 1:
+            return new Animated(sheet, "Backflip", 1);
         default:
-            return "Idle";
+            return new Animated(sheet, "Idle", 3);
         }
     }
     
     private function load(e:Event = null):Void {
         sheet = new SpriteSheet(Texture.fromBitmapData(Assets.getBitmapData("assets/Man.png")), [
-            new SpriteStrip("Walking", 0, 0, 48, 90, 1, 12, 12),
-            new SpriteStrip("Idle", 0, 90, 48, 90, 1, 7, 7)
+            new SpriteStrip("Backflip", 0, 0, 48, 90, 2, 42, 80),
+            new SpriteStrip("Walking", 0, 180, 48, 90, 1, 12, 12),
+            new SpriteStrip("Idle", 0, 270, 48, 90, 1, 7, 7)
             ]);
         
-        addChild(new Animated(sheet, randomAnimation(), 3));
+        addChild(randomAnimation());
     }
     private function add(e:MouseEvent = null):Void {
-        var a:Animated = new Animated(sheet, randomAnimation(), 3);
+        var a:Animated = randomAnimation();
         a.x = e.stageX - a.width * 0.5;
         a.y = e.stageY - a.height * 0.5;
         addChild(a);
