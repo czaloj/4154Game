a95a5093813cc044a73ee82208a8a32d7d2c9e8b
czaloj <czaloj@gmail.com>
2015-09-11 14:37:30 -0400

    Test code compilation

--- Game/src/Main.hx
+++ Game/src/Main.hx
@@ -3,12 +3,17 @@ package;
 import flash.display.Sprite;
 import flash.Lib;
 import starling.core.Starling;
+import gun.PartName;
 
 class Main extends Sprite {
     private var engine:Starling;
     
     public function new () {
         super();
+        
+        var d:Array<PartName> = [
+            new PartName("Noun", PartNameType.NOUN)
+        ];
         
         engine = new Starling(Game, Lib.current.stage);
         engine.start();
