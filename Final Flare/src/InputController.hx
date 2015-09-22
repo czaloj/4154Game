51df580556b9961887384b960811fd786ee1bea6
Mark Saad <msaad27@gmail.com>
2015-09-21 20:46:18 -0400

    More skeleton code

---
+++ Final Flare/src/InputController.hx
@@ -0,0 +1,39 @@
+package;
+
+/**
+ * ...
+ * @author Mark
+ */
+//{ Import Statements
+	
+	
+//}
+
+ 
+class InputController extends GameScreen
+{
+	//{ Fields 
+	
+	
+	//}
+
+	
+	
+	
+	//{ Initialization
+	public function new() 
+	{
+		super();
+		
+	}
+	//}
+
+
+	
+	//{ Game Loop
+	
+	
+	//}
+	
+
+}
---
+++ Final Flare/src/PlayerController.hx
@@ -0,0 +1,43 @@
+package;
+
+/**
+ * ...
+ * @author Mark
+ */
+//{ Import Statements
+	
+	
+//}
+
+ 
+class PlayerController extends GameplayController
+{
+	//{ Fields 
+	
+	
+	//}
+
+	
+	
+	
+	//{ Initialization
+	public function new() 
+	{
+		super();
+		
+	}
+	//}
+
+
+	
+	//{ Game Loop
+	
+	public function update(gameTime:GameTime):Void 
+	{
+		throw "abstract";
+	}
+	
+	//}
+	
+
+}
