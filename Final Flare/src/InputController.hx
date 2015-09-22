6e78999da6e7d1c5521d31fa4127c3cedc45462a
ishablella <iWang.wsi@gmail.com>
2015-09-21 21:57:59 -0400

    Added keyUp and keyDown handler functions to InputController

--- Final Flare/src/InputController.hx
+++ Final Flare/src/InputController.hx
@@ -9,17 +9,23 @@ package;
 	
 //}
 
- 
+/** Handlers for input events **/
 class InputController extends GameScreen
 {
-	//{ Fields 
+	// Array of booleans, indexed by key code.
+	// Element is true when corresponding key is pressed. False otherwise.
+	keysDown = [];
 	
-	
-	//}
+	// KeyDown handler. Given event e, flips key code element in keysDown to true.
+	public function keyDown(e:flash.events.KeyboardEvent):Void {
+		keysDown[e.keyCode] = true;
+	}
 
-	
-	
-	
+	// KeyUp handler. Given even e, flips key code element in keysDown to false
+	public function keyUp(e:flash.events.KeyboardEvent):Void {
+		keysDown[e.keyCode] = false;
+	}
+
 	//{ Initialization
 	public function new() 
 	{
@@ -28,12 +34,5 @@ class InputController extends GameScreen
 	}
 	//}
 
-
-	
-	//{ Game Loop
-	
-	
-	//}
-	
 
 }
