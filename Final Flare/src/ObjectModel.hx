68f9efee1e4c1c0998f5ab57715bc9875f28fdf7
Mark Saad <msaad27@gmail.com>
2015-09-21 21:56:32 -0400

    Getters and Setters

--- Final Flare/src/ObjectModel.hx
+++ Final Flare/src/ObjectModel.hx
@@ -1,6 +1,3 @@
-
-
-
 /**
  * ...
  * @author Mark
@@ -22,24 +19,96 @@ class GameplayController extends GameScreen
 	var id:String;          //Identifying tag  
 	
 	//Box2D Fields
-	var body:B2Body;
-	var bodyType:B2BodyType;
+	private var body:B2Body;
+	privatevar bodyType:B2BodyType;
 	
 	//Phsyics data
-	var position Point; 	//Object position
-	var velocity:Point; 	//Object velocity    
-	var rotation:Float; 	//Rotation	
-	var grounded:Bool;		//True if touching a platform
+	private var position Point; 	//Object position
+	private var velocity:Point; 	//Object velocity    
+	private var rotation:Float; 	//Rotation	
+	private var grounded:Bool;		//True if touching a platform
 	
 	//Drawing Fields
-	var textureSize:Point;  //Texture Size
-	var dimension:Point;    //Dimensions of box, for drawing purposes
-    var scale:Point;        //Used to scale texture
+	private var textureSize:Point;  //Texture Size
+	private var dimension:Point;    //Dimensions of box, for drawing purposes
+    private var scale:Point;        //Used to scale texture
 	
 	//}
 	
 	
 	//{Getters and Setters
+	public function getId() 
+	{
+		return id;
+	}
+	
+	public function setId(s:String) 
+	{
+		id = s;
+	}
+	
+	public function getPosition() 
+	{
+		return position;
+	}
+	public function setPosition(p:Point) 
+	{
+		position = p;
+	}
+	
+	public function getVelocity() 
+	{
+		return velocity;
+	}
+	public function setVelocity(v:Point) 
+	{
+		velocity = v;
+	}
+	
+	public function getRotation() 
+	{
+		return rotation;
+	}
+	public function setRotation(r:Float) 
+	{
+		rotation = r;
+	}
+	
+	public function getGrounded() 
+	{
+		return rotation;
+	}
+	public function setGrounded(b:Bool) 
+	{
+		grounded = b;
+	}
+	
+	public function getTextureSize() 
+	{
+		return textureSize;
+	}
+	public function setTextureSize(p:Point) 
+	{
+		textureSize = p;
+	}
+	
+	public function getDimension() 
+	{
+		return dimension;
+	}
+	public function setDimension(p:Point) 
+	{
+		dimension = p;
+	}
+	
+	public function getScale() 
+	{
+		return scale;
+	}
+	public function Scale(p:Point) 
+	{
+		scale = p;
+	}	
 	
 	//}
 	
