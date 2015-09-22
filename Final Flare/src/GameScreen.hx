e85030b1b61bf6383c37ca9063df26cd43db27e3
ishablella <iWang.wsi@gmail.com>
2015-09-21 22:44:42 -0400

    Added event listeners to GameScreen.hx

--- Final Flare/src/GameScreen.hx
+++ Final Flare/src/GameScreen.hx
@@ -3,20 +3,23 @@ package;
 import openfl.Lib;
 
 class GameScreen {
-	private var screenController:ScreenController;
-	
-	public function new() {
-		screenController = null;
-	}
-	
-	public function setParentController(sc:ScreenController):Void {
-		screenController = sc;
-	}
-	
-	public function update(gameTime:GameTime):Void {
-		throw "abstract";
-	}
-	public function draw(gameTime:GameTime):Void {
-		throw "abstract";
-	}
+    private var screenController:ScreenController;
+    
+    public function new() {
+        screenController = null;
+        inputController = InputController.new();
+        stage.addEventListener(KeyboardEvent.KEY_DOWN, inputController.keyDown);
+        stage.addEventListener(KeyboardEvent.KEY_UP, inputController.keyUp);
+    }
+    
+    public function setParentController(sc:ScreenController):Void {
+        screenController = sc;
+    }
+    
+    public function update(gameTime:GameTime):Void {
+        throw "abstract";
+    }
+    public function draw(gameTime:GameTime):Void {
+        throw "abstract";
+    }
 }
