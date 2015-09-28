a7843ba9286d08c868027a2326306593c600b63c
ch684 <ch684@cornell.edu>
2015-09-28 18:51:04 -0400

    Changed level structure and created spawner structure.

--- Final Flare/src/GameLevel.hx
+++ Final Flare/src/GameLevel.hx
@@ -1,25 +1,32 @@
 package;
 
 import openfl.Lib;
+import openfl.Assets;
+import haxe.Serializer;
+import haxe.Unserializer;
 
 class GameLevel {
-    public var foreground: Array<Array<String>>; ///<
-    public var background: Array<Array<String>>; ///<
-    public var p1: Array<Array<String>>; ///<
-    public var p2 Array<Array<String>>; ///<
-    public var p3 Array<Array<String>>; ///<
-    public var entities:Array<ObjectModel>; ///< 
-    
+    public var width:Int; //Width of level
+    public var height:Int; //Height of level
+
+    //2D array starting from upper left, coordinate (x,y) translate to index x + y * x in the array
+    public var foreground: Array<Int>; //Array of identifiers for foreground tiles
+    public var background: Array<Int>; //Array of identifiers for background tiles
+
+    public var parallax: Array<String>;//List of parallax image files, in the order of drawing
+
+    public var entities:Array<Spawner>; //List of spawn points
+
     public function new() {
         foreground = [];
-    	background = [];
-    	p1 = [];
-    	p2 = [];
-    	p3 = [];
-    	entities = [];
+        background = [];
+        parallax = [];
+        entities = [];
     }
 
-    public function createFromFile(file:String):GameLevel {
-    	return new();
+    public static function createFromFile(file:String):GameLevel {
+        var value = Assets.getText(file);
+        var unserializer = new Unserializer(value);
+        return unserializer.unserialize();
     }
 }
---
+++ Final Flare/src/Spawner.hx
@@ -0,0 +1,13 @@
+package;
+
+import openfl.geom.Point;
+
+class Spawner {
+    public var id:String;          //Identifying tag
+
+    //Physics data
+    public var position:Point = new Point();     //Spawner position
+
+    public function new() {
+    }
+}
