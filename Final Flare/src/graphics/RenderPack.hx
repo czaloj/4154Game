d1cf971e2fa1dd52caf3f431773b231bbde5df7f
czaloj <czaloj@gmail.com>
2015-09-30 09:12:45 -0400

    We don't need to store SpriteSheets for parallax, textures will do just fine

--- Final Flare/src/graphics/RenderPack.hx
+++ Final Flare/src/graphics/RenderPack.hx
@@ -1,14 +1,16 @@
 package graphics;
 
+import starling.textures.Texture;
+
 // The rendering data used by a renderer for a level
 class RenderPack {
     public var characters:SpriteSheet;
     public var enemies:SpriteSheet;
 
     public var environment:SpriteSheet;
-
-    public var parallax:Array<SpriteSheet>;
-
+    
+    public var parallax:Array<Texture>; ///< Images of the different parallax layers
+    
     public function new() {
         // Empty
     }
