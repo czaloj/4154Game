ea02ddb7d96ac10877de7a8eef67ff0d2c20dd99
czaloj <czaloj@gmail.com>
2015-09-30 09:11:10 -0400

    Fixed some spacing.
    Enemies inferred from spawners, and we can't save SpriteSheets.

--- Final Flare/src/GameLevel.hx
+++ Final Flare/src/GameLevel.hx
@@ -6,21 +6,18 @@ import openfl.Lib;
 import openfl.Assets;
 
 class GameLevel {
-    public var width:Int; //Width of level
-    public var height:Int; //Height of level
+    public var width:Int; // Width of level
+    public var height:Int; // Height of level
 
-    //2D array starting from upper left, coordinate (x,y) translate to index x + y * width in the array
-    public var foreground: Array<Int>; //Array of identifiers for foreground tiles
-    public var background: Array<Int>; //Array of identifiers for background tiles
+    // 2D array starting from upper left, coordinate (x,y) translate to index x + y * width in the array
+    public var foreground:Array<Int>; // Array of identifiers for foreground tiles
+    public var background:Array<Int>; // Array of identifiers for background tiles
 
-    public var spawners:Array<Spawner>; //List of spawn points for enemies
-    public var playerPt:Point; //Initial spawn point for the player
+    public var spawners:Array<Spawner>; // List of spawn points for enemies
+    public var playerPt:Point; // Initial spawn point for the player
 
-    public var parallax: Array<graphics.SpriteSheet>;//List of parallax image files, in the order of drawing
-    public var environmentSprites:graphics.SpriteSheet;//Sprite sheet identifier for the environment
-    public var enemiesSprites:graphics.SpriteSheet;//Sprite sheet identifier for the enemies
-
-
+    public var parallax:Array<String>; // List of parallax image files
+    public var environmentSprites:String; // Sprite sheet identifier for the environment
 
     public function new() {
         foreground = [];
