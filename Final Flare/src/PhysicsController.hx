ba96e990459c40f8ddc0655b33b03a0e42648415
czaloj <czaloj@gmail.com>
2015-10-12 00:05:29 -0400

    Remove old cruft

--- Final Flare/src/PhysicsController.hx
+++ Final Flare/src/PhysicsController.hx
@@ -9,34 +9,27 @@ import box2D.dynamics.B2World;
 import flash.display.Sprite;
 import openfl.Lib;
 
-class PhysicsController extends B2ContactListener
-{
-    private static var DT:Float = 1 / 60;
+class PhysicsController extends B2ContactListener {
     public static var GRAVITY = new B2Vec2(0,-10);
 
     public var world:B2World;
     public var debugger: B2DebugDraw;
-    public static var debug_sprite:Sprite;
 
-
-    public function new()
-    {
+    public function new() {
         super();
-        init();
-    }
 
-    private function init():Void
-    {
         world = new B2World(GRAVITY, true);
         world.setContactListener(this);
         world.setWarmStarting(true);
     }
 
-    // set debug draw
-    public function initDebug(world_sprite:openfl.display.Sprite)
-    {
+    /**
+     * Setup debugging information
+     * @param sprite The target drawing sprite
+     */
+    public function initDebug(sprite:openfl.display.Sprite) {
         var dbgDraw:B2DebugDraw = new B2DebugDraw();
-        dbgDraw.setSprite(world_sprite);
+        dbgDraw.setSprite(sprite);
         dbgDraw.setDrawScale(1.0);
         dbgDraw.setFillAlpha(0.3);
         dbgDraw.setLineThickness(1.0);
@@ -44,28 +37,23 @@ class PhysicsController extends B2ContactListener
         world.setDebugDraw(dbgDraw);
     }
 
-    public function BeginContact(contact:B2Contact): Void
-    {
-        //check what was in collision
+    public function BeginContact(contact:B2Contact):Void {
+        // Check what was in collision
         var entity1 = contact.getFixtureA().getBody().getUserData();
         var entity2 = contact.getFixtureB().getBody().getUserData();
         trace(entity1);
         trace(entity2);
 
-        if (entity1 == "player" && entity2 == "platform")
-        {
+        if (entity1 == "player" && entity2 == "platform") {
             //GameplayScreen.handlePlayerCollision();
         }
-        else if (entity1 == "platform" && entity2 == "player")
-        {
+        else if (entity1 == "platform" && entity2 == "player") {
             //GameplayScreen.handlePlayerCollision();
         }
     }
 
-
-    public function update()
-    {
-        world.step(DT, 8, 5);
+    public function update(dt:Float) {
+        world.step(dt, 8, 5);
         world.clearForces();
         world.drawDebugData();
     }
