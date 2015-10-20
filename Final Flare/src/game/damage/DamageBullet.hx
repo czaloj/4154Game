b1ea5f9d7fc9bf39177b77f74d6dc2980f085a94
czaloj <czaloj@gmail.com>
2015-10-20 19:20:40 -0400

    Add damage type enum

---
+++ Final Flare/src/game/damage/DamageBullet.hx
@@ -0,0 +1,7 @@
+package game.damage;
+
+class DamageBullet extends DamageDealer {
+    public function new() {
+        super(DamageDealer.TYPE_BULLET);
+    }
+}
--- Final Flare/src/game/damage/DamageDealer.hx
+++ Final Flare/src/game/damage/DamageDealer.hx
@@ -1,14 +1,26 @@
 package game.damage;
 
 class DamageDealer {
+    public static inline var TYPE_BULLET:Int = 1;
+    public static inline var TYPE_PIERCING_BULLET:Int = 1;
+    public static inline var TYPE_PROJECTILE:Int = 1;
+    public static inline var TYPE_RADIAL_EXPLOSION:Int = 1;
+    public static inline var TYPE_COLLISION_POLYGON:Int = 1;
+    
     public static inline var TEAM_PLAYER:Int = 0x1 << 0;
     public static inline var TEAM_ENEMY:Int = 0x1 << 1;
+    
+    public var type:Int;
     
     public var teamSourceFlags:Int; // The team that spawned this damage... if neutral, none spawned it.
     public var teamDestinationFlags:Int; // The team that should receive this damage... if neutral, all receive it.
     
     public var damage:Int; // Amount of damage to apply under normal conditions
     public var friendlyDamage:Int; // Amount of damage to apply if friendly fire occurs
+    
+    public function new(t:Int) {
+        type = t;
+    }
     
     public function damageFor(team:Int):Int {
         if ((team & teamSourceFlags) != 0) return friendlyDamage;
