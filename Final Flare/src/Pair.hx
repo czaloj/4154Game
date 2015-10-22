a823bbbbc3235adbbbace3ec8028e1a96b41c541
czaloj <czaloj@gmail.com>
2015-10-22 11:56:52 -0400

    Generic Pair class

---
+++ Final Flare/src/Pair.hx
@@ -0,0 +1,11 @@
+package;
+
+class Pair<A, B> {
+    public var first:A;
+    public var second:B;
+    
+    public function new(f:A = null, s:B = null) {
+        first = f;
+        second = s;
+    }
+}
