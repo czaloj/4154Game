bda411c660aedecee91fb6d1e065bb60fc3ebc7d
czaloj <czaloj@gmail.com>
2015-11-10 11:46:25 -0500

    disappear

--- Logging/run.ps1
+++ Logging/run.ps1
@@ -1,4 +1,4 @@
-$URL = 'gdiac.cs.cornell.edu/cs4154/fall2015/get_data.php?game_id=121&version_id=' + $args[0] + '&show_html=NO'
+$URL = 'gdiac.cs.cornell.edu/cs4154/fall2015/get_data_new.php?game_id=121&version_id=' + $args[0] + '&show_html=NO'
 ECHO $URL
 (wget $URL).Content > info.json
 .\Converter.swf info.json
