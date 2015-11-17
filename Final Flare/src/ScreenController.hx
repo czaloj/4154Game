b63564cda1fd609cdfa5293ff0cf44e3526b151a
czaloj <czaloj@gmail.com>
2015-11-17 11:42:59 -0500

    Increment to new dev version number

--- Final Flare/src/ScreenController.hx
+++ Final Flare/src/ScreenController.hx
@@ -17,8 +17,8 @@ class ScreenController extends Sprite {
     public static inline var SCREEN_HEIGHT:Int = 450;
     public static inline var VERSION_MAJOR:UInt = 0;
     public static inline var VERSION_MINOR:UInt = 1;
-    public static inline var VERSION_REVISION:UInt = 2;
-    public static inline var VERSION_ID:Int = 0 << 16 | 1 << 8 | 2;
+    public static inline var VERSION_REVISION:UInt = 5;
+    public static var VERSION_ID:Int = 0 << 16 | 1 << 8 | 2;
     public static inline var LOGGING_DEBUG_MODE:Bool = true;
 
     public var dt:GameTime = new GameTime();
