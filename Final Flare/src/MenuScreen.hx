05905f2710f599315e3c44d1186fdc83d398dc51
ch684 <ch684@cornell.edu>
2015-12-03 11:19:20 -0500

    add levels

--- Final Flare/src/MenuScreen.hx
+++ Final Flare/src/MenuScreen.hx
@@ -35,8 +35,8 @@ import starling.text.TextField;
 
 class MenuScreen extends IGameScreen {
     public static inline var LERP_SPEED:Float = .02;
-    public static var ZERO:Point = new Point(0, 0); 
-    
+    public static var ZERO:Point = new Point(0, 0);
+
     public static var HOME_POS_MIN:Point = new Point(30, 609);
     public static var HOME_POS_MAX:Point = new Point(829, 1058);
     public static var LEVEL_SELECT_POS_MIN:Point = new Point(22, 17);
@@ -47,22 +47,22 @@ class MenuScreen extends IGameScreen {
     public static var TEMP_MAX:Point = new Point(1680, 1079);
     //public static var SHOP_POS_MIN:Point = new Point(1399, 791);
     //public static var SHOP_POS_MAX:Point = new Point(1874, 1058);
- 
-    
+
+
     //Current position of camera
     public var currentMin:Point = new Point();
     public var currentMax:Point = new Point();
     public var currentWindow:Point = new Point();
     public var window:Point = new Point();
-    
+
     //Distance to translate each frame
     public var delta:Point = new Point();
     public var deltaScale:Point = new Point();
-    
+
     //Total distance to translate before transistionDone is marked as true
     public var distance:Point = new Point();
     public var transitionDone:Bool = true;
-    
+
     //UIPanes
     private var mainMenu:UIPane;
     private var homePane:UIPane; //This didn't need any special functionality so it's a generic pane
@@ -70,14 +70,14 @@ class MenuScreen extends IGameScreen {
     private var loadoutPane:LoadoutPane;
     private var shopPane:UIPane;
 
-    private var uif:UISpriteFactory;  //Buttons are created from UISpriteFactory    
+    private var uif:UISpriteFactory;  //Buttons are created from UISpriteFactory
     private var backGround:Image;     //Background
     private var confirmButton:Button; //For squad select pane
-    
+
     public function new(sc:ScreenController) {
         super(sc);
     }
-    
+
     override public function build():Void {
         // Empty
     }
@@ -86,9 +86,9 @@ class MenuScreen extends IGameScreen {
     }
 
     override public function onEntry(gameTime:GameTime):Void {
-        screenController.playerData = new PlayerData("Player"); // TODO: Allow others to play?        
+        screenController.playerData = new PlayerData("Player"); // TODO: Allow others to play?
         uif = new UISpriteFactory(Texture.fromBitmapData(Assets.getBitmapData("assets/img/UI.png")));
-        
+
         currentMin.setTo(0, 0);
         currentMax.setTo(800, 450);
         delta.setTo(0, 0);
@@ -152,7 +152,7 @@ class MenuScreen extends IGameScreen {
         //Add the background
         backGround = new Image(Texture.fromBitmapData(Assets.getBitmapData("assets/img/MenuScreens.png")));
         screenController.addChild(backGround);
-        
+
         //INIT HOME PANE
         //Set up formatting stuff
         var mainBTF:ButtonTextFormat = {
@@ -186,10 +186,10 @@ class MenuScreen extends IGameScreen {
         });
         tutorialButton.bEvent.add(function():Void {
             screenController.loadedLevel = LevelCreator.loadLevelFromFile("assets/level/tutorial.lvl");
-            screenController.switchToScreen(2); 
+            screenController.switchToScreen(2);
         });
         shopButton.bEvent.add(transitionToShop);
-        
+
         //Initialize UIPane and add buttons
         homePane = new UIPane();
 
@@ -198,23 +198,23 @@ class MenuScreen extends IGameScreen {
         homePane.add(tutorialButton, 56 + tutorialButton.width, 410 - playButton.height);
         homePane.add(shopButton, 84 + 2 * shopButton.width, 410 - playButton.height);
         homePane.add(levelEditorButton, 112 + 3 * levelEditorButton.width, 410 - playButton.height);
-        
-        //Vertical Button Layout 
+
+        //Vertical Button Layout
         /*
         homePane.add(playButton, 400 + 2*playButton.width / 3, 60);
         homePane.add(tutorialButton, 400 + 2*playButton.width / 3, 70 + tutorialButton.height);
         homePane.add(shopButton, 400 + 2*playButton.width / 3, 90 + 2 * shopButton.height);
         homePane.add(levelEditorButton, 400 + 2 * playButton.width / 3, 100 + 3 * shopButton.height);
         */
-                
+
         //INIT LEVEL SELECT PANE
         levelSelectPane = new LevelSelectPane();
         levelSelectPane.menuButton.bEvent.add(transitionToHome);
         levelSelectPane.confirmButton.bEvent.add(transitionToLoadout);
-        
+
         //INIT LOADOUT PANE
         loadoutPane = new LoadoutPane();
-        
+
         //Set up formatting stuff
         var loadoutBTF:ButtonTextFormat = {
             tx:100,
@@ -230,12 +230,12 @@ class MenuScreen extends IGameScreen {
         //Create confirm button and add functionality
         confirmButton = uif.createButton(100, 35, "CONFIRM", loadoutBTF, false);
         confirmButton.bEvent.add(startLevel);
-        
+
         loadoutPane.add(confirmButton, 400 - confirmButton.width / 2, 375);
-        
+
         //INIT SHOP PANE
         shopPane = new UIPane();
-        
+
         //Set up formatting stuff
         var shopBTF:ButtonTextFormat = {
             tx:200,
@@ -247,7 +247,7 @@ class MenuScreen extends IGameScreen {
             hAlign:HAlign.CENTER,
             vAlign:VAlign.CENTER
         };
-        
+
         var btf:ButtonTextFormat = {
             tx:100,
             ty:35,
@@ -258,16 +258,16 @@ class MenuScreen extends IGameScreen {
             hAlign:HAlign.CENTER,
             vAlign:VAlign.CENTER
         };
-        
+
         //Add UI elements
         var evolution = new ui.ShopElement("Evolution Points: ", 2000);
         var shadiness = new ui.ShopElement("Shadiness Points: ", 2000);
         var historical = new ShopElement("Historical Points: ", 2000);
         confirmButton = uif.createButton(200, 35, "GENERATE WEAPON", shopBTF, false);
         var menuButton = uif.createButton(100, 35, "MAIN MENU", btf, false);
-        //confirmButton.bEvent.add(generateWeapon); 
+        //confirmButton.bEvent.add(generateWeapon);
         menuButton.bEvent.add(transitionToHome);
-        
+
         shopPane.add(evolution, 400 - evolution.width/2, 120);
         shopPane.add(shadiness, 400 - shadiness.width/2, 205);
         shopPane.add(historical, 400 - historical.width / 2, 280);
@@ -279,11 +279,11 @@ class MenuScreen extends IGameScreen {
         mainMenu.add(levelSelectPane, LEVEL_SELECT_POS_MIN.x, LEVEL_SELECT_POS_MIN.y);
         mainMenu.add(loadoutPane, LOADOUT_POS_MIN.x, LOADOUT_POS_MIN.y);
         mainMenu.add(shopPane, TEMP_MIN.x, TEMP_MIN.y);
-        
+
         screenController.addChild(mainMenu);
 
     }
-    
+
     //TODO
     //Transitions the screen to a rectangle and zooms appropriately
     public function transitionToRect(min:Point, max:Point) {
@@ -292,14 +292,14 @@ class MenuScreen extends IGameScreen {
         currentWindow.setTo(currentMax.x - currentMin.x, currentMax.y - currentMin.y);
         window.setTo(max.x - min.x, max.y - min.y);
         deltaScale.setTo(currentWindow.x / window.x, currentWindow.y / window.y);
-        
-        
+
+
         currentMin = min;
         currentMax = max;
 
         transitionDone = false;
     }
-    
+
     public function updateCamera():Void {
         if (!transitionDone) {
             mainMenu.transformationMatrix.translate(delta.x, delta.y);
@@ -311,13 +311,13 @@ class MenuScreen extends IGameScreen {
                 backGround.transformationMatrix.translate( -distance.x, -distance.y);
                 transitionDone = true;
 
-            }            
+            }
         }
     }
-    
+
     private function createInputTextField(height:Float, width:Float, x:Float = 0, y:Float = 0, font:String, fontSize:Float, fontColor:UInt, bg:Bool, bgColor:UInt = null):Sprite {
         var sprite:Sprite = new Sprite();
-        
+
         // Create default text format
         var textFormat:TextFormat = new TextFormat(font, fontSize, fontColor);
         textFormat.align = TextFormatAlign.CENTER;
@@ -335,7 +335,7 @@ class MenuScreen extends IGameScreen {
         sprite.addChild(tf);
         return sprite;
     }
-    
+
     private function transitionToHome():Void {
         transitionToRect(HOME_POS_MIN.clone(), HOME_POS_MAX.clone());
     }
@@ -347,11 +347,11 @@ class MenuScreen extends IGameScreen {
     private function transitionToLoadout():Void {
         transitionToRect(LOADOUT_POS_MIN.clone(), LOADOUT_POS_MAX.clone());
     }
-    
+
     private function transitionToShop():Void {
         transitionToRect(TEMP_MIN.clone(), TEMP_MAX.clone());
     }
-    
+
     //TODO change to BroadcastEvent1 with a string argument for level
     private function startLevel():Void {
         var i = 0;
@@ -359,11 +359,15 @@ class MenuScreen extends IGameScreen {
             screenController.playerData.selectedChars.push((loadoutPane.selected[i]));
             i++;
         }
-        
+
         switch levelSelectPane.selectedLevel {
             case 0:
-                screenController.loadedLevel = LevelCreator.loadLevelFromFile("assets/level/easy.lvl");
+                screenController.loadedLevel = LevelCreator.loadLevelFromFile("assets/level/test.lvl");
             case 1:
+                screenController.loadedLevel = LevelCreator.loadLevelFromFile("assets/level/caged4.lvl");
+            case 2:
+                screenController.loadedLevel = LevelCreator.loadLevelFromFile("assets/level/easy.lvl");
+            case 3:
                 screenController.loadedLevel = LevelCreator.loadLevelFromFile("assets/level/medium.lvl");
             default:
         }
